545cde4c341e9654e87a0df2cb41e22c3c1e030d
Renato Martins <renatto.martins@gmail.com>
2017-08-14 23:07:53 -0300

    Add initial install scripts for tests

---
+++ openrc.sh
@@ -0,0 +1,7 @@
+export OS_AUTH_URL=http://127.0.0.1/identity
+export OS_TENANT_NAME="admin"
+export OS_USERNAME="admin"
+export OS_PASSWORD="secret"
+export OS_REGION_NAME="RegionOne"
+export OS_ENDPOINT_TYPE=publicURL
+export OS_IDENTITY_API_VERSION=2
